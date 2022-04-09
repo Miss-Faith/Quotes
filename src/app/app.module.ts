5d07b5c2eed452eb3615572ab37302cd403980f4
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-09 23:59:39 +0300

    Add countclicks directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { CountclicksDirective } from './countclicks.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    CountclicksDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/countclicks.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appCountclicks]'
+})
+export class CountclicksDirective {
+
+  constructor() { }
+
+  numberOfClicks = 0;
+
+  @HostListener("click", ["$event.target"]) onClick(btn) {
+    this.numberOfClicks++
+  }
+ 
+}
