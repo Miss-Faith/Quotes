5d63c2a785808351aae8d642cb0b154d87212029
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-11 05:40:16 +0300

    change date settings

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quotes'
 export class QuotesComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1,"Mi Amor","The big lesson in life, baby, is never be scared of anyone or anything.", "Frank Sinatra", new Date(2021,17,12),0,0),
-    new Quote(2,"Mi Amor","The big lesson in life, baby, is never be scared of anyone or anything.", "Frank Sinatra", new Date(2021,17,12),0,0),
-    new Quote(3,"Mi Amor","The big lesson in life, baby, is never be scared of anyone or anything.", "Frank Sinatra", new Date(2021,17,12),0,0)
+    new Quote(1,"Mi Amor","The big lesson in life, baby, is never be scared of anyone or anything.", "Frank Sinatra", new Date(2021,16,20),0,0),
+    new Quote(2,"Mi Amor","When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.", "John Lennon", new Date(2021,17,11),0,0),
+    new Quote(3,"Mi Amor","Too many of us are not living our dreams because we are living our fears.", "Les Brown", new Date(2022,4,11),0,0)
   ];
 
   addNewQuote(quote) {
